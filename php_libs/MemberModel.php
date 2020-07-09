5a53660f7221d2f8a7715820d183a89906a89598
AkariUC <akari@pluslab.org>
2020-07-09 12:39:06 +0900

    ADD MemberController.php MemberModel.php

---
+++ php_libs/MemberModel.php
@@ -0,0 +1,128 @@
+<?php
+
+class MemberModel extends BaseModel {
+    //----------------------------------------------------
+    // 会員登録処理
+    //----------------------------------------------------
+    public function regist_member($userdata){
+        try {
+            $this->pdo->beginTransaction();
+            $sql = "INSERT  INTO member (username, password, last_name, first_name, birthday, ken, reg_date )
+            VALUES ( :username, :password, :last_name, :first_name, :birthday, :ken , now() )";
+            $stmh = $this->pdo->prepare($sql);
+            $stmh->bindValue(':username',   $userdata['username'],   PDO::PARAM_STR );
+            $stmh->bindValue(':password',   $userdata['password'],   PDO::PARAM_STR );
+            $stmh->bindValue(':name',       $userdata['name'],       PDO::PARAM_STR );
+            $stmh->execute();
+            $this->pdo->commit();
+        } catch (PDOException $Exception) {
+            $this->pdo->rollBack();
+            print "エラー：" . $Exception->getMessage();
+        }
+    }
+
+
+    //----------------------------------------------------
+    // 会員のユーザ名（メールアドレス）と同じものがないか調べる。
+    //----------------------------------------------------
+    public function check_username($userdata){
+        try {
+            $sql= "SELECT * FROM member WHERE username = :username ";
+            $stmh = $this->pdo->prepare($sql);
+            $stmh->bindValue(':username',  $userdata['username'], PDO::PARAM_STR );
+            $stmh->execute();
+            $count = $stmh->rowCount();
+        } catch (PDOException $Exception) {
+            print "エラー：" . $Exception->getMessage();
+        }
+        if($count >= 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    //----------------------------------------------------
+    // 会員情報をユーザー名（メールアドレス）で検索
+    //----------------------------------------------------
+    public function get_authinfo($username){
+        $data = [];
+        try {
+            $sql= "SELECT * FROM member WHERE username = :username limit 1";
+            $stmh = $this->pdo->prepare($sql);
+            $stmh->bindValue(':username',  $username,  PDO::PARAM_STR );
+            $stmh->execute();
+            $data = $stmh->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $Exception) {
+            print "エラー：" . $Exception->getMessage();
+        }
+        return $data;
+    }
+
+
+    //----------------------------------------------------
+    // 会員情報をユーザーIDで検索
+    //----------------------------------------------------
+    public function get_member_data_id($id){
+        $data = [];
+        try {
+            $sql= "SELECT * FROM member WHERE id = :id limit 1";
+            $stmh = $this->pdo->prepare($sql);
+            $stmh->bindValue(':id', $id, PDO::PARAM_INT );
+            $stmh->execute();
+            $data = $stmh->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $Exception) {
+            print "エラー：" . $Exception->getMessage();
+        }
+        return $data;
+    }
+
+
+    //----------------------------------------------------
+    // 会員情報の更新処理
+    //----------------------------------------------------
+    public function modify_member($userdata){
+        try {
+            $this->pdo->beginTransaction();
+            $sql = "UPDATE  member
+                    SET 
+                        username   = :username,
+                        password   = :password,
+                        name       = :name,
+                    WHERE id = :id";
+            $stmh = $this->pdo->prepare($sql);
+            $stmh->bindValue(':username',   $userdata['username'],   PDO::PARAM_STR );
+            $stmh->bindValue(':password',   $userdata['password'],   PDO::PARAM_STR );
+            $stmh->bindValue(':name',       $userdata['name'],       PDO::PARAM_STR );
+            $stmh->bindValue(':id',         $userdata['id'],         PDO::PARAM_INT );
+            $stmh->execute();
+            $this->pdo->commit();
+            //print "データを" . $stmh->rowCount() . "件、更新しました。<br>";
+        } catch (PDOException $Exception) {
+            $this->pdo->rollBack();
+            print "エラー：" . $Exception->getMessage();
+        }
+    }
+
+
+    //----------------------------------------------------
+    // 会員情報を完全に削除する
+    //----------------------------------------------------
+    public function delete_member($id){
+        try {
+            $this->pdo->beginTransaction();
+            $sql = "DELETE FROM member WHERE id = :id";
+            $stmh = $this->pdo->prepare($sql);
+            $stmh->bindValue(':id', $id, PDO::PARAM_INT );
+            $stmh->execute();
+            $this->pdo->commit();
+            //print "データを" . $stmh->rowCount() . "件、削除しました。<br>";
+        } catch (PDOException $Exception) {
+            $this->pdo->rollBack();
+            print "エラー：" . $Exception->getMessage();
+        }
+    }
+    
+    
+}
